0786d526cdc5fad61bbc02ce6d79beddb9ae5ab9
Irvin Lim <irvinlimweiquan@gmail.com>
2023-02-27 08:31:51 +0800

    chore: Fix GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
-import { defineConfig } from 'vite';
-import postcss from './postcss.config.js';
-import { svelte } from '@sveltejs/vite-plugin-svelte';
+import { defineConfig } from "vite";
+import postcss from "./postcss.config.js";
+import { svelte } from "@sveltejs/vite-plugin-svelte";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,4 +8,7 @@ export default defineConfig({
   css: {
     postcss,
   },
+
+  // https://vitejs.dev/guide/static-deploy.html
+  base: "/cpm-calculator/",
 });
